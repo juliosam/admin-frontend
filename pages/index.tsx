2994785e94fe40f418e4ca08dff21fc65d88057e
Julio <ja.samaniegom@gmail.com>
2022-08-07 23:53:35 -0500

    first MIU

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,31 @@
 import type { NextPage } from 'next'
 import { Button } from '@mui/material'
-import { ShoppingCartRounded } from '@mui/icons-material'
+import PersonIcon from '@mui/icons-material/Person';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import Shop2Icon from '@mui/icons-material/Shop2';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
+
   return (
     <div className='home'>
-      Hello world!
-      <div>
-        <Button variant="text" startIcon={<ShoppingCartRounded />}>
-          Add to Cart
-        </Button>
-        <Button variant="contained" startIcon={<ShoppingCartRounded />}>
-          Add to Cart
-        </Button>
-        <Button variant="outlined" startIcon={<ShoppingCartRounded />}>
-          Add to Cart
-        </Button>
+      <h1>ADMON SOFT</h1>
+      <div className='button-section'>
+        <Link href={`/products/`}>
+          <Button variant="contained" endIcon={<InventoryIcon sx={{width: 30, height:30}}/>}>
+            Products
+          </Button>
+        </Link>
+        <Link href={`/users/`}>
+          <Button variant="contained" endIcon={< PersonIcon sx={{width: 30, height:30}}/>}>
+            Users
+          </Button>
+        </Link>
+        <Link href={`/orders/`}>
+          <Button variant="contained" endIcon={< Shop2Icon sx={{width: 30, height:30}}/>}>
+            Orders
+          </Button>
+        </Link>
       </div>
     </div>
   )
